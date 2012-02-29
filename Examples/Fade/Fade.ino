78a8069cf69e6b0872e1af91fbb241ab3967a1b0
Alexander Brevig <alexanderbrevig@gmail.com>
2012-02-29 14:53:20 +0100

    Fix comments from Blink to Fade

--- Examples/Fade/Fade.ino
+++ Examples/Fade/Fade.ino
@@ -10,8 +10,9 @@ void loop() {
   byte start_color3[] = {255, 0, 0};
   byte end_color3[] = {0, 255, 255};
   
-  // Blink
+  // Fade from start to end
   LED1.Fade(LEDArray, start_color3 , end_color3, 5); 
   delay(100);
+  // Fade from end to start
   LED1.Fade(LEDArray, end_color3 , start_color3, 5);
 }
